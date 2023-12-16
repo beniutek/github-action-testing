c48df5402c52ba3985c3eee21371229a0e0ee99d
beniutek <bbeniutek@gmail.com>
2023-12-16 19:53:52 +0000

    build: add completely new writer options

--- release.config.js
+++ release.config.js
@@ -20,55 +20,77 @@ const commitAnalyzerPlugin = [
 // Custom writer options for the release notes and changelog
 const customWriterOpts = {
   transform: (commit, context) => {
-    const issues = [];
+    const issues = []
 
-    // Custom handling for 'build' type commits
-    if (commit.type === 'build') {
-      commit.type = 'Build';
-    } else if (commit.type === 'feat') {
-      commit.type = 'Features';
-    } else if (commit.type === 'fix') {
-      commit.type = 'Bug Fixes';
-    } else if (commit.type === 'perf') {
-      commit.type = 'Performance Improvements';
-    } else if (commit.type === 'revert') {
-      commit.type = 'Reverts';
-    } else if (!commit.type || commit.type === 'chore') {
-      // Any other type of commit is ignored
+    const allowedCommitTypes = {
+      'feat': 'Features',
+      'fix': 'Bug Fixes',
+      'perf': 'Performance Improvements',
+      'revert': 'Reverts',
+      'docs': 'Documentation',
+      'style': 'Styles',
+      'refactor': 'Code Refactoring',
+      'test': 'Tests',
+      'build': 'Build System',
+      'ci': 'Continuous Integration'
+    };
+
+    commit.notes.forEach(note => { note.title = 'BREAKING CHANGES' })
+
+    if (allowedCommitTypes[commit.type]) {
+      commit.type = allowedCommitTypes[commit.type];
+    } else {
       return;
     }
 
     if (commit.scope === '*') {
-      commit.scope = '';
+      commit.scope = ''
     }
 
     if (typeof commit.hash === 'string') {
-      commit.shortHash = commit.hash.substring(0, 7);
+      commit.shortHash = commit.hash.substring(0, 7)
     }
 
     if (typeof commit.subject === 'string') {
       let url = context.repository ? `${context.host}/${context.owner}/${context.repository}` : context.repoUrl;
+
       if (url) {
-        url = `${url}/issues/`;
+        url = `${url}/issues/`
         // Issue URLs.
         commit.subject = commit.subject.replace(/#([0-9]+)/g, (_, issue) => {
-          issues.push(issue);
-          return `[#${issue}](${url}${issue})`;
-        });
+          issues.push(issue)
+          return `[#${issue}](${url}${issue})`
+        })
+      }
+
+      if (context.host) {
+        // User URLs.
+        commit.subject = commit.subject.replace(/\B@([a-z0-9](?:-?[a-z0-9/]){0,38})/g, (_, username) => {
+          if (username.includes('/')) {
+            return `@${username}`
+          }
+
+          return `[@${username}](${context.host}/${username})`
+        })
       }
     }
 
-    // Remove references that already appear in the subject
+    // remove references that already appear in the subject
     commit.references = commit.references.filter(reference => {
       if (issues.indexOf(reference.issue) === -1) {
-        return true;
+        return true
       }
 
-      return false;
-    });
+      return false
+    })
 
-    return commit;
-  }
+    return commit
+  },
+  groupBy: 'type',
+  commitGroupsSort: 'title',
+  commitsSort: ['scope', 'subject'],
+  noteGroupsSort: 'title',
+  notesSort: compareFunc
 };
 
 module.exports = {
